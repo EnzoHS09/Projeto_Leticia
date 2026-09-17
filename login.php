806c293ab1fe4187baead36ccc0fb27ad8d69a4b
danmf18 <danilo.m.filho@aluno.senai.br>
2026-09-17 14:16:17 -0300

    login admin

---
+++ login.php
@@ -0,0 +1,55 @@
+<?php
+require_once('data/crud.php');
+
+session_start();
+
+$erro = null;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $usuarioencontrado = read($pdo, 'admin', "email = " . $pdo->quote($email));
+
+    if ($usuarioencontrado && $usuarioencontrado['senha'] === $senha) {
+
+        $_SESSION['user_id']   = $usuarioencontrado['id_admin'];
+        $_SESSION['user_name'] = $usuarioencontrado['nome_admin'];
+        $_SESSION['user_tipo'] = 'admin';
+
+        header('location: admin.php');
+        exit;
+    } else {
+        $erro = 'Email ou senha incorretos.';
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+
+<body>
+
+    <form method="POST" action="login.php">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" required><br>
+
+    <?php if ($erro) {
+        echo "<p style='color: red;'>$erro</p>";
+    } ?>
+        <input type="submit" value="Login">
+    </form>
+
+</body>
+
+</html>
